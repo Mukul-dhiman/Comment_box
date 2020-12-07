35a89061a257be3c2ddc5295f8676063cd798e43
Mukul-dhiman <orton1615@gmail.com>
2020-12-07 08:07:07 +0530

    connecting empty database named comment_box_db

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask,render_template
+from flask_mysqldb import MySQL
+# import MySQLdb
 
 app=Flask(__name__)
+
+import yaml
+db = yaml.load(open('db.yaml'))
+app.config['MYSQ_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db'] 
+mysql = MySQL(app)
+
+
 
 @app.route('/')
 def home():
