fceaf129403eb50fbd995ca0ed5025b1fc98500d
Mukul-dhiman <orton1615@gmail.com>
2020-12-08 09:15:50 +0530

    set up simple search

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ def Result(cursor,query):
     query = query.strip().split(" ")
     result = ()
     for word in query:
-        cursor.execute("select comment_id from comment where comments_text regexp '(^|[[:space:]])"+str(word)+"([[:space:]]|$)'")
+        cursor.execute("select * from comment where comments_text regexp '(^|[[:space:]])"+str(word)+"([[:space:]]|$)'")
         word_result = cursor.fetchall()
         result = result + word_result
     return result
